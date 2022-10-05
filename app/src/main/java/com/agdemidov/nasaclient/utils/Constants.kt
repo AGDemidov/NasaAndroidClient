85a5654704175a2e410abdecc22c5a435a6d954c
AGDemidov <dremich@yandex.ru>
2022-10-05 13:06:48 +0300

    Added screenshots for readme

--- app/src/main/java/com/agdemidov/nasaclient/utils/Constants.kt
+++ app/src/main/java/com/agdemidov/nasaclient/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.agdemidov.nasaclient.utils
 
 const val NASA_BASE_API_URL = "https://api.nasa.gov"
-const val NASA_API_KEY = "EQ2XBWrcHQ0o5E62ghA23xbGc9oQLdEg9NBep1Vu"
+const val NASA_API_KEY = "DEMO_KEY"
 const val PAGE_SIZE = 48
 const val PROGRESS_START_POS = 0
 const val PROGRESS_END_POS = 150
